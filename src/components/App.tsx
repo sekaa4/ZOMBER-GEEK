f6389b94e5874420ea8d26ddc060bee900820224
SeregeyIankowski <garret933@gmail.com>
2023-01-31 11:31:30 +0300

    feat: add module resolution to adding photos in project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,12 @@
+import photo from "../assets/phone.png";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <>
+      <h1>Hello world</h1>;
+      <img src={photo} alt="some" />
+    </>
+  );
 }
 
 export default App;
