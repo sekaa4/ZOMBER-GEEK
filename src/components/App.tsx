e61769472ddd48ee0ac9d2c13764848ebf3f6fd3
SergeyIankowski <92864057+SergeyIankowski@users.noreply.github.com>
2023-02-01 18:39:18 +0300

    feat: create webpack confing for react (#1)
    * feat: create webpack confing for react
    
    * feat: add eslint and prettier to project
    
    * reafactor: change readme file
    
    * feat: add module resolution to adding photos in project

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import photo from "../assets/phone.png";
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>;
+      <img src={photo} alt="some" />
+    </>
+  );
+}
+
+export default App;
