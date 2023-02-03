4f39e648c26ce3303a2dd53d748c0ccb73921142
Sergey P <s.pansevi4@gmail.com>
2023-02-03 18:36:34 +0300

    fix: change config setup for work with css modules

--- src/module.d.ts
+++ src/module.d.ts
@@ -4,3 +4,10 @@ declare module "*.gif";
 declare module "*.png";
 declare module "*.svg";
 declare module "*.webp";
+declare module "*.scss" {
+  interface ClassNames {
+    [className: string]: string;
+  }
+  const classNames: ClassNames;
+  export = classNames;
+}
