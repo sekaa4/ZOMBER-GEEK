d66579c23daf127bebcd38b41c9558cc18e3e576
SeregeyIankowski <garret933@gmail.com>
2023-02-11 15:08:57 +0300

    feat: create gameBoard container

--- src/containers/GameBoard/GameBoard.tsx
+++ src/containers/GameBoard/GameBoard.tsx
@@ -3,9 +3,15 @@ import FieldCell from "../../components/Field-cell/FieldCell";
 import classes from "./GameBoard.module.scss";
 
 const GameBoard: FC = () => {
-  const cellsData = new Array(144).fill(<FieldCell />);
+  const cellsData = new Array(144).fill(1);
 
-  return <div className={classes.gameField}>{cellsData}</div>;
+  return (
+    <div className={classes.gameField}>
+      {cellsData.map((item, index) => (
+        <FieldCell key={`${`${index}a`}`} />
+      ))}
+    </div>
+  );
 };
 
 export default GameBoard;
