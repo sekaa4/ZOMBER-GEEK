7a04eab7470b086d0a35f4ede8ba1182fbb847c4
SeregeyIankowski <garret933@gmail.com>
2023-02-11 15:07:52 +0300

    refactor: delete comments

--- src/components/characterCard/CharacterCard.tsx
+++ src/components/characterCard/CharacterCard.tsx
@@ -1,14 +1,6 @@
 import { FC } from "react";
 import classes from "./characterCard.module.scss";
 
-// interface CharacterCardProps {
-//   id: number;
-//   left: boolean;
-//   right: boolean;
-//   top: boolean;
-//   bottom: boolean;
-// }
-
 const CharacterCard: FC = () => {
   return <div className={classes.characterCard} />;
 };
