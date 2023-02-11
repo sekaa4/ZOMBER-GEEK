754f077ed29f65cc5a4391a35ce2f2a447dc9063
SeregeyIankowski <garret933@gmail.com>
2023-02-11 15:14:00 +0300

    refactor: delete comments

--- src/components/Field-cell/FieldCell.tsx
+++ src/components/Field-cell/FieldCell.tsx
@@ -2,7 +2,6 @@ import { FC } from "react";
 import classes from "./field-cell.module.scss";
 
 const FieldCell: FC = () => {
-  // const [st, useSt] = useState(0);
   return <div className={classes.fieldCell} />;
 };
 
