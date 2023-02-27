0dfd9b84684bfab8ab50038f0cbf9cf101c74ea3
SeregeyIankowski <garret933@gmail.com>
2023-02-27 15:09:21 +0300

    feat: change initial values of items

--- src/constants/Items.ts
+++ src/constants/Items.ts
@@ -2,8 +2,8 @@ import Items from "../entities/Items/Items";
 import ItemsAndWeaponsNames from "../models/ItemsAndWeaponsNames";
 
 const items = new Items({
-  [ItemsAndWeaponsNames.FIRST_AID_KITS]: 6,
-  [ItemsAndWeaponsNames.BOARDS]: 8,
+  [ItemsAndWeaponsNames.FIRST_AID_KITS]: 14,
+  [ItemsAndWeaponsNames.BOARDS]: 0,
   [ItemsAndWeaponsNames.GASOLINE]: 1,
   [ItemsAndWeaponsNames.KEYS]: 1,
 });
