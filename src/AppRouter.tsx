98ae425c5a53a246d3d19e6bc7a25656af1d7009
ImmelstronDev <99259052+ImmelstronDev@users.noreply.github.com>
2023-02-28 22:50:50 +0300

    feat: added check token on front (#34)

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -9,15 +9,37 @@ import Developers from "./pages/Developers/Developers";
 import Welcome from "./pages/Welcome";
 
 const AppRouter = () => {
+  const token = document.cookie;
   return (
     <Routes>
-      <Route path={Pages.welcome} element={<Welcome />} />
-      <Route path={Pages.main} element={<Main />} />
+      <Route path={Pages.notfound} element={<Welcome />} />
+      {token !== "isToken=undefined" && token !== "" ? (
+        <>
+          <Route path={Pages.welcome} element={<Main />} />
+          <Route path={Pages.main} element={<Main />} />
+          <Route path={Pages.create} element={<CreateGame />} />
+          <Route path={Pages.game} element={<BoardPage />} />
+          <Route path={Pages.login} element={<Main />} />
+          <Route path={Pages.signup} element={<Main />} />
+          <Route path={Pages.developers} element={<Developers />} />
+        </>
+      ) : (
+        <>
+          <Route path={Pages.welcome} element={<Welcome />} />
+          <Route path={Pages.create} element={<Welcome />} />
+          <Route path={Pages.main} element={<Welcome />} />
+          <Route path={Pages.game} element={<Welcome />} />
+          <Route path={Pages.developers} element={<Welcome />} />
+          <Route path={Pages.login} element={<LoginPage />} />
+          <Route path={Pages.signup} element={<SignUpPage />} />
+        </>
+      )}
+      {/* <Route path={Pages.main} element={<Main />} />
       <Route path={Pages.create} element={<CreateGame />} />
       <Route path={Pages.game} element={<BoardPage />} />
       <Route path={Pages.login} element={<LoginPage />} />
       <Route path={Pages.signup} element={<SignUpPage />} />
-      <Route path={Pages.developers} element={<Developers />} />
+      <Route path={Pages.developers} element={<Developers />} /> */}
     </Routes>
   );
 };
